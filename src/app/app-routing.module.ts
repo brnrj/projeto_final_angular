b406e82d09737bb893a08e189cc080d489888ea9
Bruno Martins <bruno.martins217@gmail.com>
2021-12-09 12:57:29 -0300

    criando as rotas do modulo dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardRoutes } from './dashboard';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: '/dashboard',
-  //   pathMatch: 'full'
-  // }
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  ...DashboardRoutes
 ];
 
 @NgModule({
