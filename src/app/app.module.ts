38cef3dd2cf884a6ac98f8d8e9ebe6b40bcb1715
Bruno Martins <bruno.martins217@gmail.com>
2021-12-09 12:59:13 -0300

    integrando o modulo calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard';
+import { CalculadoraModule } from './calculadora';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { DashboardModule } from './dashboard';
   imports: [
     BrowserModule,
     DashboardModule,
+    CalculadoraModule,
     AppRoutingModule
   ],
   providers: [],
